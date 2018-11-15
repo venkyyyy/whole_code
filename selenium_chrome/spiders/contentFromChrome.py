92ea7ffe4d0513ea22aa5bf8ffc4930f6d2656cb
venkyyyy <venkyanthony@gmail.com>
2018-11-15 11:43:25 +0530

    Update contentFromChrome.py

--- selenium_chrome/spiders/contentFromChrome.py
+++ selenium_chrome/spiders/contentFromChrome.py
@@ -18,7 +18,7 @@ class ContentfromchromeSpider(scrapy.Spider):
         yield scrapy.Request(url='https://www.google.com/search?q=jobs+near+me&ibp=htl;jobs', callback=self.parse)
     
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path='D://chromedriver.exe')
+        self.driver = webdriver.Chrome()
 
     def parse(self, response):
         self.driver.get('https://www.google.com/search?q=jobs+near+me&ibp=htl;jobs')
